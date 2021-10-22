9f5e0b9a341ee5bd870f1e76a32f9bc66ea44b1b
Joentze <Joentze01@gmail.com>
2021-10-22 00:38:50 -0700

    chembuild api test

--- ChemicalStructureConstructor/Quark-API.js
+++ ChemicalStructureConstructor/Quark-API.js
@@ -31,6 +31,7 @@ window.addEventListener('message',event =>{
 function organiseObject(){
     console.log("getting object....2")
     let obj = {atoms:structure.atoms}
+    console.log(structure.atoms.prototype)
     console.log(obj)
     return obj
 }
